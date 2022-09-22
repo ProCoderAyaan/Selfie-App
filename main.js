7999282089fc896bf7c647d9f4b2ed094b9f7dbd
ProCoderAyaan <95852791+ProCoderAyaan@users.noreply.github.com>
2022-09-22 18:52:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,66 @@
-var speechRecognition = window.webkitSpeechRecognition
-var recognition = new speechRecognition
-function start() {
-    document.getElementById("textbox").innerHTML = "";
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
     recognition.start();
-}
-recognition.onresult() = function(event){
-    console.log(event);
-    var Content = event.results[0][0].transcript;
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    if(Content == "Take My Selfie"){
-        console.log("Taking Selfie ---");
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
         speak();
-    }
+      }
 }
-function speak() {
+
+
+function speak(){
     var synth = window.speechSynthesis;
-    speak_data = "Taking your Selfie in 5 seconds";
+
+    speak_data = "Taking you Selfie in 5 seconds";
+
     var utterThis = new SpeechSynthesisUtterance(speak_data);
+
     synth.speak(utterThis);
-    Webcab.attach(camera);
-    setTimeout(function(){
-    take_snapshot();
-    save();
-},5000);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
 }
+
+ 
 camera = document.getElementById("camera");
 Webcam.set({
-    width: 320,
-    height: 240,
-    image_format: 'jpeg',
-    jpeg_quality: 90
- });
- function take_snapshot() { Webcam.snap(function(data_uri) {
-     document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>'; 
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
+}
+
+function save() {
+    link = document.getElementById("link");
+    Image = document.getElementById("selfie_image").src;
+    link.href = Image;
+    link.click();
 }
